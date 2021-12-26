d29491fc7fd4bcad6ba2d28ba792d0af6652442e
hieuldt <huyy@gft.com>
2021-12-26 13:46:59 +0700

    adding github automation

---
+++ github-automation.tf
@@ -0,0 +1,26 @@
+resource "spacelift_stack" "github_automation" {
+  administrative = false
+  autodeploy     = false
+
+  name           = "github-automation"
+  description    = ""
+  manage_state   = false
+#   worker_pool_id = "01FQ0MDWV3B44RGJ7QBV0K59HB"
+
+  repository = "github-automation-2"
+  branch     = "main"
+
+  terraform_version = var.terraform_version
+  runner_image      = var.spacelift_runner_image
+
+  before_init = [
+    "git config --global url.\"https://foo:$${GITHUB_TOKEN}@github.com/hieu-ldt\".insteadOf \"https://github.com/hieu-ldt\""
+  ]
+  after_init = [
+    "terraform fmt -recursive -check",
+    "tflint --init",
+    "tflint --disable-rule=terraform_module_pinned_source",
+    "tfsec .",
+    "checkov -d . --framework=terraform --quiet"
+  ]
+}
